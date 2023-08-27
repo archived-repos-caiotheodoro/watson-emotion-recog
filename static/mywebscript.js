a66a8e9e271152d5f0c4a51574b34469b7c32dae
Caio Theodoro <dev.caiotheodoro@gmail.com>
2023-08-26 21:35:59 -0300

    feat: 🎸 initial commit

--- static/mywebscript.js
+++ static/mywebscript.js
@@ -5,7 +5,7 @@ let RunSentimentAnalysis = ()=>{
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("system_response").innerHTML = xhttp.responseText;
-        }
+        } 
     };
     xhttp.open("GET", "emotionDetector?textToAnalyze"+"="+textToAnalyze, true);
     xhttp.send();
